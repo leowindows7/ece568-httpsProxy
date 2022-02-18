46ecf90574ee6991e38a6c0bc6f051ad17cf304e
Ian <cl583@duke.edu>
2022-02-17 20:45:41 -0500

    dev: build connect method skeletion with several issue 1. after sending 200 ok to client, client send weird response to proxy 2. after sending the weird response from client, response is forward to server and the program block at poll

--- src/client.cpp
+++ src/client.cpp
@@ -19,20 +19,8 @@
  **/
 std::string Client::connectToHost(std::string hostname,
                                   int port_num,
-                                  std::string http_request) {
-  Network * client_connect = cconnect.get();
-  std::pair<int, struct addrinfo *> socketInfo =
-      client_connect->connectSetup<int, struct addrinfo *>(hostname.c_str(), port_num);
-
-  int socket_fd = socketInfo.first;
-  struct addrinfo * serviceinfo = socketInfo.second;
-
-  if (connect(socket_fd, serviceinfo->ai_addr, serviceinfo->ai_addrlen) == -1) {
-    // TOOO: refactor to throw exception
-    perror("connect");
-    throw std::exception();
-  }
-
+                                  std::string http_request,
+                                  int socket_fd) {
   Network::sendRequest(socket_fd, http_request.c_str(), http_request.size());
 
   std::string response = Network::recvRequest(socket_fd);
@@ -58,4 +46,21 @@ std::string Client::connectToHost(std::string hostname,
   std::cout << validResponse << std::endl << validResponse.size() << std::endl;
 
   return validResponse;
+}
+
+int Client::setUpSocket(std::string hostname, int port_num) {
+  Network * client_connect = cconnect.get();
+  std::pair<int, struct addrinfo *> socketInfo =
+      client_connect->connectSetup<int, struct addrinfo *>(hostname.c_str(), port_num);
+
+  int socket_fd = socketInfo.first;
+  struct addrinfo * serviceinfo = socketInfo.second;
+
+  if (connect(socket_fd, serviceinfo->ai_addr, serviceinfo->ai_addrlen) == -1) {
+    // TOOO: refactor to throw exception
+    perror("connect");
+    throw std::exception();
+  }
+
+  return socket_fd;
 }
